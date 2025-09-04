8c8f08c3e7bd1dcd88f106516eb94059b88ada96
EugeneKornev <yevgene.kornev@gmail.com>
2025-09-04 20:19:57 +0700

    Solution5

--- Task_1_1_1/src/test/java/HeapSortTest.java
+++ Task_1_1_1/src/test/java/HeapSortTest.java
@@ -1,5 +1,5 @@
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import org.junit.jupiter.api.Test;
 
 /**
  * Test class for HeapSort implementation.
--- Task_1_1_1/src/test/java/MainTest.java
+++ Task_1_1_1/src/test/java/MainTest.java
@@ -1,7 +1,7 @@
-import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
